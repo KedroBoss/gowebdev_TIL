8936056e296f4810ac53a37fd022a81a22ed7fae
KedroBoss <dimmak90@gmail.com>
2017-08-25 10:03:27 +0300

    02-mux_routing

--- servers/routing/main.go
+++ servers/routing/main.go
@@ -21,7 +21,11 @@ func main() {
 	var handle handleHandler
 
 	mux := http.NewServeMux()
+
+	// /hand/moreURL will be handled by the Handle
 	mux.Handle("/hand/", hand)
+
+	// /handle only be handled
 	mux.Handle("/handle", handle)
 	http.ListenAndServe(":8080", mux)
 }
