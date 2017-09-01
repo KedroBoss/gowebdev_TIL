c327414c99ce01edeec3a91b9c00ca59136591d5
KedroBoss <dimmak90@gmail.com>
2017-09-01 16:27:23 +0300

    expiring cookie

--- state/cookies/visit_counter/main.go
+++ state/cookies/visit_counter/main.go
@@ -6,12 +6,16 @@ import (
 	"strconv"
 )
 
+// MaxAge sets the expiration time for a cookie
+// To delete is: use MaxAge: -1
+
 func increment(w http.ResponseWriter, req *http.Request) {
 	c, err := req.Cookie("counter")
 	if err == http.ErrNoCookie {
 		c = &http.Cookie{
-			Name:  "counter",
-			Value: "0",
+			Name:   "counter",
+			Value:  "0",
+			MaxAge: 15,
 		}
 	}
 	tmp, _ := strconv.Atoi(c.Value)
