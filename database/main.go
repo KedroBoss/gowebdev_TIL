b7e54924b95b7b7896233e9685291be2c513c6e0
KedroBoss <dimmak90@gmail.com>
2017-09-25 15:02:07 +0300

    db: insert fields

--- database/main.go
+++ database/main.go
@@ -4,12 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"io"
+	"html/template"
 	"net/http"
 )
 
 var db *sql.DB
 var err error
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
 
 func main() {
 	db, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/test_schema?charset=utf8")
@@ -26,8 +32,45 @@ func main() {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	_, err = io.WriteString(w, "Connected!")
+
+	if req.Method == http.MethodPost {
+		name := req.FormValue("cName")
+		stmt, err := db.Prepare(`INSERT INTO customer SET cName=?`)
+		if err != nil {
+			http.Redirect(w, req, "/", http.StatusInternalServerError)
+			fmt.Println(err.Error())
+			return
+		}
+
+		r, err := stmt.Exec(name)
+		if err != nil {
+			http.Redirect(w, req, "/", http.StatusInternalServerError)
+			fmt.Println(err.Error())
+			return
+		}
+		n, err := r.RowsAffected()
+		if err != nil {
+			http.Redirect(w, req, "/", http.StatusInternalServerError)
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(n)
+	}
+
+	rows, err := db.Query(`SELECT cName FROM customer`)
 	check(err)
+
+	var (
+		name  string
+		names []string
+	)
+
+	for rows.Next() {
+		err = rows.Scan(&name)
+		check(err)
+		names = append(names, name)
+	}
+	tpl.ExecuteTemplate(w, "list.html", names)
 }
 
 func check(e error) {
