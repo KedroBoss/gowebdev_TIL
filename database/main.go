8eae1e7007c87cd4a52dfc4a2f0f81fa77462ad6
KedroBoss <dimmak90@gmail.com>
2017-09-25 13:44:08 +0300

    db: MySQL connection

---
+++ database/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"io"
+	"net/http"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/test_schema?charset=utf8")
+	check(err)
+	defer db.Close()
+
+	err = db.Ping()
+	check(err)
+
+	http.HandleFunc("/", index)
+	http.Handle("/favicon", http.NotFoundHandler())
+	err := http.ListenAndServe(":8080", nil)
+	check(err)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	_, err = io.WriteString(w, "Connected!")
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		fmt.Println(e)
+	}
+}
